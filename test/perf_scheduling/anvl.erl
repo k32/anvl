00d42f3d03506b3112fdc3fcc97677c9a6bf91a1
k32 <10274441+k32@users.noreply.github.com>
2025-10-03 17:14:28 +0200

    Add a performance test

---
+++ test/perf_scheduling/anvl.erl
@@ -0,0 +1,25 @@
+-include("anvl.hrl").
+
+conf() ->
+  #{ conditions => [run]
+   }.
+
+?MEMO(run,
+      begin
+        N = 100000,
+        Depth = 5,
+        precondition(child(Depth, N))
+      end).
+
+?MEMO(child, N, Depth,
+      case Depth of
+        0 ->
+          false;
+        _ ->
+          precondition(children(N, Depth - 1, []))
+      end).
+
+children(N, _, Acc) when N =< 0 ->
+  Acc;
+children(N, Depth, Acc) ->
+  children(N - 1, Depth, [child(N, Depth) | Acc]).
