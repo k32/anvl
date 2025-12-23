122b2e25ed0ed8bb932a064d44def44956cca5de
k32 <10274441+k32@users.noreply.github.com>
2025-12-23 10:09:48 +0100

    Fix dummy test

--- test/dummy/anvl.erl
+++ test/dummy/anvl.erl
@@ -1,7 +1,36 @@
+-include("anvl.hrl").
+
 conf() ->
   #{ plugins => [anvl_erlc]
    , conditions => [default]
    }.
 
+init() ->
+  anvl_erlc:add_pre_compile_hook(
+    anvl_project:root(),
+    fun(#{src_root := SrcRoot}) ->
+        precondition(sources_created(SrcRoot, 1000))
+    end).
+
 default() ->
   anvl_erlc:app_compiled(default, dummy).
+
+?MEMO(sources_created, Dir, N,
+      begin
+        newer([], filename(Dir, N)) andalso
+          begin
+            lists:foreach(
+              fun(I) ->
+                  FN = filename(Dir, I),
+                  ok = file:write_file(FN, ["-module(", mod_name(I), ").\n"])
+              end,
+              lists:seq(1, N)),
+            true
+          end
+      end).
+
+filename(Dir, N) ->
+  filename:join([Dir, "src", mod_name(N) ++ ".erl"]).
+
+mod_name(N) ->
+  "dummy_" ++ integer_to_list(N).
